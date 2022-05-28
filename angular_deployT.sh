922aa90548ae0e7c70c85879ceccdb42ca3a2d39
Mohan125 <53725713+Mohan125@users.noreply.github.com>
2022-05-28 16:41:12 +0530

    Update angular_deployT.sh

--- angular_deployT.sh
+++ angular_deployT.sh
@@ -1,6 +1,6 @@
 #! bin/bash
 
-sshpass -p "admin" scp -r dist ubuntu@ec2-13-233-190-244.ap-south-1.compute.amazonaws.com:/opt/tomcat/webapps
+sshpass -p "admin" scp -r dist ubuntu@ec2-35-154-130-138.ap-south-1.compute.amazonaws.com:/opt/tomcat/webapps
 #g
 
-sshpass -p "admin" ssh ubuntu@ec2-13-233-190-244.ap-south-1.compute.amazonaws.com "cd /opt/tomcat/webapps;mv dist $1;cd $1;cp -r $2/. ."
+sshpass -p "admin" ssh ubuntu@ec2-35-154-130-138.ap-south-1.compute.amazonaws.com "cd /opt/tomcat/webapps;mv dist $1;cd $1;cp -r $2/. ."
