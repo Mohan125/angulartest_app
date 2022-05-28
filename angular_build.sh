f578077fae751a46f2bad2584d31af0963b3d6da
Mohan125 <53725713+Mohan125@users.noreply.github.com>
2022-05-28 10:18:25 +0530

    Update angular_build.sh

--- angular_build.sh
+++ angular_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -v $1:/usr/src/myangular -w /usr/src/myangular -ti node:16-alpine sh -c "npm install @angular/CLI;npm install;npm run build -- --base-href=/$2/"
+docker run -v $1:/usr/src/myangular -w /usr/src/myangular node:16-alpine sh -c "npm install @angular/CLI;npm install;npm run build -- --base-href=/$2/"
 
 ls
 
